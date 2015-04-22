2d659c4c3795b85c7f188b10d307a17b0ba51d66
Jainish Shah <Jainishshah@yahoo.com>
2015-04-22 10:36:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -29,14 +29,14 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err,db){
                 whitespacePattern = /^\s*$/;
 
             if(whitespacePattern.test(name) || whitespacePattern.test(message)){
-
-               sendStatus('name and message is required');
+               sendStatus('Name and message is required');
             }else{
-                col.insert({name : name, message :message}, function(){
+                col.insert({name: name, message: message}, function(){
+
                     //emit latest message to all clients
                     client.emit('output', [data]);
 
-                    setStatus({
+                    sendStatus({
                         message:"Message Sent",
                         clear :true
                     });
